73d95cec1af40620301f744ff7fb06a6276b5608
Valentina Chandía <artemisiaklartraum@gmail.com>
2024-07-11 17:53:03 -0400

    se agrega animaciones y js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a.nav-link').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+          window.scrollTo({
+            top: target.offsetTop - 50, 
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+  
   
